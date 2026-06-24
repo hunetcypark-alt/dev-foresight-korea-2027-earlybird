667fce21c5a897afeb2535d288ce7263b506a49f
cypark <hunetcypark@gmail.com>
2026-06-24 13:33:24 +0900

    fix: vite base path를 /dev-foresight-korea-2027-earlybird/로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/foresight-korea-2027-earlybird/',
+  base: '/dev-foresight-korea-2027-earlybird/',
 })
